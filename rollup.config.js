0c9305be9e2884113ee202e88c3a5f7c69bbea83
samme <samme@users.noreply.github.com>
2024-06-30 16:16:37 -0700

    Remove buble, update phaser

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,5 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
-import buble from 'rollup-plugin-buble';
 import pkg from './package.json';
 
 export default [
@@ -13,8 +12,7 @@ export default [
     external: ['phaser'],
     plugins: [
       resolve(),
-      commonjs(),
-      buble({ exclude: ['node_modules/**'] })
+      commonjs()
     ]
   }
 ];
